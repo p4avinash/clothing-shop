9eb7fc26d930e87af83550a74e0f5bde79574b4e
Avinash <p4avinashmaurya@gmail.com>
2020-07-21 19:56:51 +0530

    added more to the layout

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -8,32 +8,32 @@ class Directory extends Component {
     this.state = {
       sections: [
         {
-          title: "hats",
+          title: "HATS",
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
           id: 1,
           linkUrl: "shop/hats",
         },
         {
-          title: "jackets",
+          title: "JACKETS",
           imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
           id: 2,
           linkUrl: "shop/jackets",
         },
         {
-          title: "sneakers",
+          title: "SNEAKERS",
           imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
           id: 3,
           linkUrl: "shop/sneakers",
         },
         {
-          title: "womens",
+          title: "WOMEN",
           imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
           size: "large",
           id: 4,
           linkUrl: "shop/womens",
         },
         {
-          title: "mens",
+          title: "MEN",
           imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
           size: "large",
           id: 5,
