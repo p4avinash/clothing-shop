727765be5686b4d81700e120e71dcdfa81746516
Avinash <p4avinashmaurya@gmail.com>
2020-07-25 06:58:32 +0530

    added the navigation to the hats page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
+import { Route } from "react-router-dom";
+
+const HatsPage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>This is the hats page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Route exact path="/" component={Homepage} />
+      <Route exact path="/shop/hats" component={HatsPage} />
     </div>
   );
 }
--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./menu-item-style.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+import { withRouter } from "react-router-dom";
+
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => {
   return (
-    <div className={`${size} menu-item`}>
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
@@ -16,4 +21,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
