bd9f0f99a1c7099a1b21deaeec98584fd41c1521
Avinash <p4avinashmaurya@gmail.com>
2020-07-26 07:50:54 +0530

    added the shop preview page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
+import ShopPage from "./pages/shops/shop-page";
 import { Route } from "react-router-dom";
-
-const HatsPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>This is the hats page</h1>
-    </div>
-  );
-};
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={Homepage} />
-      <Route exact path="/shop/hats" component={HatsPage} />
+      <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }
