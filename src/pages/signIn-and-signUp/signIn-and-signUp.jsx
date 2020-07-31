418374f90a780e42e1bac58f3f2acb23109f1184
Avinash <p4avinashmaurya@gmail.com>
2020-07-31 17:49:25 +0530

    added the sign-up component and stores the data on firebase

--- src/pages/signIn-and-signUp/signIn-and-signUp.jsx
+++ src/pages/signIn-and-signUp/signIn-and-signUp.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./signIn-and-signUp.scss";
 
 import SignIn from "../../components/sign-in/sign-in";
+import SignUp from "../../components/sign-up/sign-up";
 
 const SignInAndSignUp = () => {
   return (
     <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
